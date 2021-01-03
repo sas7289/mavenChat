efcf299376f918d2de8d37db3fb42142063cd892
Alex Shakhov <sas7289@yandex.ru>
2021-01-03 19:09:44 +0300

    tests for transArr complete

--- HomeTask/src/test/java/TransArrayTest.java
+++ HomeTask/src/test/java/TransArrayTest.java
@@ -1,18 +1,27 @@
 
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransArrayTest {
+    private TransArray transArray;
+
+    @BeforeEach
+    void startUp() {
+        transArray = new TransArray();
+    }
+
 
     @DisplayName("Without 4")
     @Test
     public void testTransArr1() {
         Integer[] testArr = new Integer[] {1,2,3,5,6,7,8,9};
-        TransArray transArray = new TransArray();
 
         Assertions.assertThrows(RuntimeException.class, () -> {
             transArray.transArr(testArr);
@@ -24,7 +33,6 @@ class TransArrayTest {
     public void testTransArr2() {
         Integer[] testArr = new Integer[] {1,2,3,4,5,6,7,8,9};
         Integer[] targetArr = new Integer[] {5,6,7,8,9};
-        TransArray transArray = new TransArray();
 
         Assertions.assertArrayEquals(targetArr, transArray.transArr(testArr));
 
@@ -38,7 +46,6 @@ class TransArrayTest {
     public void testTransArr3() {
         Integer[] testArr = new Integer[] {4,5,1,9,5,4,7,4};
         Integer[] targetArr = new Integer[]{};
-        TransArray transArray = new TransArray();
 
         Assertions.assertArrayEquals(targetArr, transArray.transArr(testArr));
 
@@ -52,12 +59,33 @@ class TransArrayTest {
     public void testTransArr4() {
         Integer[] testArr = new Integer[] {1,2,3,4,6,4,7,3,12,3};
         Integer[] targetArr = new Integer[] {7,3,12,3};
-        TransArray transArray = new TransArray();
 
         Assertions.assertArrayEquals(targetArr, transArray.transArr(testArr));
 
         Assertions.assertDoesNotThrow(() -> {
             transArray.transArr(testArr);
         });
+    }
+
+
+    @DisplayName("ParamTest")
+    @ParameterizedTest
+    @MethodSource("getArguments")
+    public void testTransParam (Integer[] targetArr, Integer[] testArr) {
+        Assertions.assertArrayEquals(targetArr, transArray.transArr(testArr));
+    }
+
+    static Stream<Arguments> getArguments() {
+        Integer[] testArr2 = new Integer[] {1,2,3,4,5,6,7,8,9};
+        Integer[] testArr3 = new Integer[] {4,5,1,9,5,4,7,4};
+        Integer[] testArr4 = new Integer[] {1,2,3,4,6,4,7,3,12,3};
+        Integer[] targetArr2 = new Integer[] {5,6,7,8,9};
+        Integer[] targetArr3 = new Integer[]{};
+        Integer[] targetArr4 = new Integer[] {7,3,12,3};
+        return Stream.of(
+                Arguments.arguments(targetArr2, testArr2),
+                Arguments.arguments(targetArr3, testArr3),
+                Arguments.arguments(targetArr4, testArr4)
+        );
     }
 }
